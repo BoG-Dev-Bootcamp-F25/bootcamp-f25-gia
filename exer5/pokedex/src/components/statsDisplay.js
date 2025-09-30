512a56f73ca13e4c73bca33d414581cb118e5d21
giannacastr <62898067+giannacastr@users.noreply.github.com>
2025-09-30 16:08:32 -0400

    formatted statsDisplay

--- exer5/pokedex/src/components/statsDisplay.js
+++ exer5/pokedex/src/components/statsDisplay.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import './styles/statsDisplay.css'
 
 function StatsDisplay({ pokemonData }) {
     return (
         <div className="stats-panel">
-            <h4>Info</h4>
-            <p>height: {pokemonData.height / 10}m</p>
-            <p>weight: {pokemonData.weight / 10}kg</p>
-        
-            {pokemonData.stats.map(statObject => (
-                <p key={statObject.stat.name}>
-                    {statObject.stat.name}: {statObject.base_stat}
-                </p>
-            ))}
+            <h4 id="stats-title">Info</h4>
+            <div className="general-stats">
+                <p>height: {pokemonData.height / 10}m</p>
+                <p>weight: {pokemonData.weight / 10}kg</p>
+            
+                {pokemonData.stats.map(statObject => (
+                    <p key={statObject.stat.name}>
+                        {statObject.stat.name}: {statObject.base_stat}
+                    </p>
+                ))}
+            </div>
         </div> 
     );
 }

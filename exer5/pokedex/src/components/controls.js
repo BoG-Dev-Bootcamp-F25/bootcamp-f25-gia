6b2fa9087d43d743e91dcfc6ddcbbec26ca21b46
giannacastr <62898067+giannacastr@users.noreply.github.com>
2025-09-30 15:54:24 -0400

    i figured it out without that weird module thing

--- exer5/pokedex/src/components/controls.js
+++ exer5/pokedex/src/components/controls.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import leftArrow from './left.png'
-import rightArrow from './right.png'
-import styles from './styles/controls.module.css'
+import React from 'react';
+import left from './left.png';
+import right from './right.png';
+import './styles/controls.css';
 
-// fix appearance on the buttons later so no margins between them and img
+// fix appearance on the buttons later so no margins between them
 function Controls({ onPrevClick, onNextClick }) {
     return (
-        <div>
-            <button className={styles.changeButtons} onClick={onPrevClick}>
-                <img src={leftArrow} style={{ width: '70px'}}/>
+        <div id="change-buttons">
+            <button className="arrow-button" onClick={onPrevClick}>
+                <img src={left} style={{ width: '70px'}}/>
             </button>
-            <button className={styles.changeButtons} onClick={onNextClick}>
-                <img src={rightArrow} style={{ width: '70px', margin: '0px'}}/>
+            <button className="arrow-button" onClick={onNextClick}>
+                <img src={right} style={{ width: '70px', margin: '0px'}}/>
             </button>
         </div>
     );

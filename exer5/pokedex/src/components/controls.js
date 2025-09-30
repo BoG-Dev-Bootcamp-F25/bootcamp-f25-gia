605a0450d00d80d847ac763246535e62974f9417
giannacastr <62898067+giannacastr@users.noreply.github.com>
2025-09-30 15:07:03 -0400

    everything is the same but i learned about the module css for react

--- exer5/pokedex/src/components/controls.js
+++ exer5/pokedex/src/components/controls.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import leftArrow from './left.png'
 import rightArrow from './right.png'
+import styles from './styles/controls.module.css'
 
-// fix appearance on the buttons later so no margins between them and img refs
+// fix appearance on the buttons later so no margins between them and img
 function Controls({ onPrevClick, onNextClick }) {
     return (
-        <div id="change-buttons"
-        style = {{ margin: '10px' }}>
-            <button class="arrow-button" style={{ border: 'none', background: 'white', margin: '5px' }} onClick={onPrevClick}>
+        <div>
+            <button className={styles.changeButtons} onClick={onPrevClick}>
                 <img src={leftArrow} style={{ width: '70px'}}/>
             </button>
-            <button class="arrow-button" style={{ border: 'none', background: 'white', margin: '5px' }} onClick={onNextClick}>
+            <button className={styles.changeButtons} onClick={onNextClick}>
                 <img src={rightArrow} style={{ width: '70px', margin: '0px'}}/>
             </button>
         </div>
--- exer5/pokedex/src/components/statsDisplay.js
+++ exer5/pokedex/src/components/statsDisplay.js
@@ -6,6 +6,12 @@ function StatsDisplay({ pokemonData }) {
             <h4>Info</h4>
             <p>height: {pokemonData.height / 10}m</p>
             <p>weight: {pokemonData.weight / 10}kg</p>
+        
+            {pokemonData.stats.map(statObject => (
+                <p key={statObject.stat.name}>
+                    {statObject.stat.name}: {statObject.base_stat}
+                </p>
+            ))}
         </div> 
     );
 }
